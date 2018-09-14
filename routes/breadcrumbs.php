54524542d9bab14ba9bba0babdb44565b37fb85a
Mironchikov Ilya <i.mironchikov@gmail.com>
2018-09-14 16:59:18 +0300

    redis

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -8,3 +8,8 @@ Breadcrumbs::for('learn', function ($trail) {
     $trail->parent('home', route('home'));
     $trail->push('Learn', route('learn'));
 });
+
+Breadcrumbs::for('blog', function ($trail) {
+    $trail->parent('home', route('home'));
+    $trail->push('Blog', route('blog'));
+});
